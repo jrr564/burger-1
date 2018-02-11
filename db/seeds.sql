aadf6f7d4ca51bc40e9d26d7682fc4be0fa066db
Steve Zinski <szinski@Steves-iMac.fios-router.home>
2018-02-10 21:32:31 -0500

    Getting things into place

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Seeds
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Double');
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
